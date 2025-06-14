96169a1427a67bfb1a48984f26bf63186714a45d
Vidusavindu <savindujob@gmail.com>
2025-06-14 17:35:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "BmVQCKwT#6l4Kzi2HT7DG5oq_Qn_GbhBf9vQUMJGbb4ZruOhI-Xw":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "wvtSTJgK#twWrSYW-986FpDIE6lsS-Z7LIbKCWQmtudl2x33DURY":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "mongodb://mongo:lgsbZRFLSAekRXNmLAfExvcDcGuZeyoC@yamanote.proxy.rlwy.net:49819",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
